c13c3a53e0a9396513079149aeb58375243e1573
Sooraj <soorajsm56@gmail.com>
2025-05-02 16:09:49 +0530

    Terraform code to create a vm in gcp

---
+++ creating_vm_custom_vpc/main.tf
@@ -0,0 +1,49 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.8.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "terraform-network"
+  auto_create_subnetworks = false
+}
+
+
+# Create a Subnet
+resource "google_compute_subnetwork" "subnet_1" {
+  name          = "subnet-1"
+  ip_cidr_range = "10.0.0.0/24"
+  region        = "us-central1"
+  network       = google_compute_network.vpc_network.id
+}
+
+# Create a VM Instance
+resource "google_compute_instance" "vm_instance" {
+  name         = "my-vm"
+  machine_type = "e2-micro"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network    = google_compute_network.vpc_network.id
+    subnetwork = google_compute_subnetwork.subnet_1.id
+    access_config {} # This gives it a public IP
+  }
+
+  tags = ["web"]
+}
---
+++ creating_vm_default_vpc/variable.tf
@@ -0,0 +1,23 @@
+variable "instance_name_value" {
+  description = "name of the instance"
+  type        = string
+}
+
+variable "machine_type_value" {
+  description = "machine type of the instance"
+  type        = string
+}
+
+variable "zone_value" {
+  description = "zone of the instance"
+  type        = string
+}
+
+variable "image_value" {
+  description = "image of the instance"
+  type        = string
+}
+
+variable "project_id" {}
+variable "region" {}
+
